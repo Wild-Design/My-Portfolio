2ad584f3c046851e86f10a8492ef4c8e7bf300f2
Wild-Design <brenneke_ruger@hotmail.com>
2024-02-19 13:14:05 -0300

    change colors clear theme

--- src/utils/themes.ts
+++ src/utils/themes.ts
@@ -9,21 +9,21 @@ export const darkTheme: ThemeConfig = {
   colorDescription: '#aeb7d5',
   colorProject: '#2A3362',
   skillColor: '#13465d',
-  colorForm: '#4C7EB5',
+  colorForm: '#96c4ec',
   border: '#fff',
   icon: '#fff',
 };
 
 export const clearTheme: ThemeConfig = {
-  firstBackground: '#E6E9F2 ',
-  secondBackground: '#F0F2F8 ',
-  thirdBackground: '#BFE1F1',
+  firstBackground: '#D4E4F7',
+  secondBackground: '#B5AFA6',
+  thirdBackground: '#a0d8ee',
   colorTitle: '#2A3362',
   colorSubtitle: '#2A3362',
   colorDescription: '#13465d',
   colorProject: '#9f9fe2',
-  skillColor: '#888',
-  colorForm: '#13465d',
-  border: '#2A3362',
+  skillColor: '#a0d8ee',
+  colorForm: '#547796',
+  border: 'transparent',
   icon: '#2A3362',
 };
